e30ff000c574171a63b344fd92833608e99a1a98
DonThePhan <donthephan@gmail.com>
2022-09-25 17:40:25 -0400

    add basic google calendar to webpage

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque explicabo neque itaque dolorum a eius! Sequi minima eos, tempora nesciunt sapiente sit blanditiis provident doloribus aliquam sunt laborum accusamus rem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet est iure dolores natus pariatur, nihil facere perferendis culpa similique sint incidunt illo id quas necessitatibus cumque reprehenderit quis officia at.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, corporis eius? Ducimus dignissimos eos possimus, asperiores iusto quo non consequatur, recusandae tempore a mollitia necessitatibus praesentium at placeat quasi doloremque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla excepturi veniam reprehenderit ipsum consequuntur aspernatur dolorem eligendi iure impedit sapiente sint perspiciatis neque veritatis vitae debitis, esse id modi. Itaque.
+      </div>
+      <div style={{ display: 'flex' }}>
+        <iframe
+          src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FToronto&src=ZG9udGhlcGhhbkBnbWFpbC5jb20&src=dTh2dm9sZ2tkM283dHE2ZXUwODc1ZGoya2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=cG9nbHFhMmJxcWJqYmc5MThiYzdicTZrdDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=cHJmNWt1bTM3NDA0aGk2ZG1rcDc4NDdndnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=Y180c2FtMGJqMnF0MjJncTdoM3YxYm1mdG9sNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%23E67C73&color=%23F6BF26&color=%23D50000&color=%23AD1457'
+          style={{ border: 'solid 1px #777', width: '50%', height: '100vh' }}
+          // width='800'
+          frameBorder='0'
+          scrolling='no'
+        ></iframe>
+        <iframe
+          src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FToronto&showNav=1&showDate=1&showPrint=0&showTabs=1&showCalendars=1&showTz=1&mode=AGENDA&src=ZG9udGhlcGhhbkBnbWFpbC5jb20&src=dTh2dm9sZ2tkM283dHE2ZXUwODc1ZGoya2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=cG9nbHFhMmJxcWJqYmc5MThiYzdicTZrdDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=cHJmNWt1bTM3NDA0aGk2ZG1rcDc4NDdndnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=Y180c2FtMGJqMnF0MjJncTdoM3YxYm1mdG9sNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%23E67C73&color=%23F6BF26&color=%23D50000&color=%23AD1457'
+          style={{ border: 'solid 1px #777', width: '50%', height: '100vh' }}
+          frameborder='0'
+          scrolling='no'
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default App;
